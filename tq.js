b4c83432a331bec0c933b50c2d218871d05caf75
Thejas Harshitha <Thejas Harshitha@DESKTOP-BV7P0JK>
2025-01-02 09:59:15 +0530

    Add project files.

---
+++ tq.js
@@ -0,0 +1,14 @@
+﻿document.getElementById('responseForm').addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevent the form from submitting to a server
+
+    // Get form values
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const review = document.getElementById('review').value;
+
+    // You can add validation or any other logic here if needed
+
+    // Hide the form and show the thank you message
+    document.getElementById('responseForm').style.display = 'none';
+    document.getElementById('thankYouMessage').style.display = 'block';
+});
